0c835bc579cf64f7faac23644840adc9755ca7a5
pkncoder <alex.h.preston@gmail.com>
2023-10-04 15:17:50 -0400

    Added coments, if you click the button it prints

--- ButtonDoThing.java
+++ ButtonDoThing.java
@@ -1,26 +1,63 @@
+// AWT stuff
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+// Swing imports
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class ButtonDoThing extends JFrame implements ActionListener
 {
-    ButtonDoThing() {
 
-        JButton button = new JButton();
+    JButton button;
+
+    public void createButton()
+    {
+        // Create button
+        button = new JButton("Click Me!!");
+
+        // Give button coords and size and stuff (actually thats it)
         button.setBounds(200, 200, 100, 50);
 
+        // Give the button an action listener
+        button.addActionListener(this);
+
+        // Give it a close operation so we can click the red X
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Give it a layout
         this.setLayout(null);
+
+        // Give the window a size
         this.setSize(500, 500);
+
+        // Shows the window
         this.setVisible(true);
+
+        // Add the button to the window
         this.add(button);
 
     }
 
-    public void ActionListener(ActionEvent e) {
+    public static void main(String[] args) 
+    {
+        // Create an object so we can run commands in this main file
+        ButtonDoThing start = new ButtonDoThing();
 
-
+        // Start it all up
+        start.createButton();
     }
+
+    // This tests for actions
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        // Test for button action
+        if (e.getSource()==button)
+        {
+            // Print out something
+            System.out.println("You clicked me!");
+        }
+    }
+
 }
--- HelloSwing.java
+++ HelloSwing.java
@@ -1,4 +1,3 @@
-import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
